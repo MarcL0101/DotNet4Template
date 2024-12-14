f33b1bd30fb89a3d77f60cd8c1c100d9747abfbe
Marc-Tan'Sky Larochelle <larochelle.ma@csdm.qc.ca>
2024-12-13 19:51:04 -0500

    Ajoutez des fichiers projet.

---
+++ DotNet4Template/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNet4Template.Startup))]
+namespace DotNet4Template
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
